32fe293745304469dce717047d3625c187c42317
Adrian Lupu <adrianvlupu@gmail.com>
2019-05-31 20:12:19 +0300

    still no working postinstall script

--- scripts/get.js
+++ scripts/get.js
@@ -46,22 +46,23 @@ plantuml.testdot(function (isOk) {
             console.error('Unsupported operating system for V8 jars.')
         }
       }
-    })
+    });
+
+    var PACKAGE_JSON_PATH = path.join(__dirname, '../package.json')
+    var plantumlVersion = require(PACKAGE_JSON_PATH).plantumlVersion
+
+    var JAR_DIR_PATH = path.join(__dirname, '../vendor')
+    var PLANTUML_JAR = path.join(JAR_DIR_PATH, 'plantuml.jar')
+
+    var PLANTUML_FILES_URL = 'https://sourceforge.net/projects/plantuml/files/'
+    var PLANTUML_FILES_JAR_PATH = plantumlVersion + '/plantuml.' + plantumlVersion + '.jar'
+
+    if (!fs.existsSync(JAR_DIR_PATH)) {
+      fs.mkdirSync(JAR_DIR_PATH)
+    }
+
+    console.log('Downloading plantuml.jar version ' + plantumlVersion)
+    download(PLANTUML_FILES_URL + PLANTUML_FILES_JAR_PATH, PLANTUML_JAR, true)
   }
 })
 
-var PACKAGE_JSON_PATH = path.join(__dirname, '../package.json')
-var plantumlVersion = require(PACKAGE_JSON_PATH).plantumlVersion
-
-var JAR_DIR_PATH = path.join(__dirname, '../vendor')
-var PLANTUML_JAR = path.join(JAR_DIR_PATH, 'plantuml.jar')
-
-var PLANTUML_FILES_URL = 'https://sourceforge.net/projects/plantuml/files/'
-var PLANTUML_FILES_JAR_PATH = plantumlVersion + '/plantuml.' + plantumlVersion + '.jar'
-
-if (!fs.existsSync(JAR_DIR_PATH)) {
-  fs.mkdirSync(JAR_DIR_PATH)
-}
-
-console.log('Downloading plantuml.jar version ' + plantumlVersion)
-download(PLANTUML_FILES_URL + PLANTUML_FILES_JAR_PATH, PLANTUML_JAR, true)
